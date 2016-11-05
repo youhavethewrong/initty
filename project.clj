a53466200c22e0c946776942f29e2496f1b6288d
ESC <youhavethewrong@gmail.com>
2016-11-05 14:30:56 -0500

    Add licensing information

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject initty "0.1.0-SNAPSHOT"
+  :description "A D20 initiative tracker."
+  :url "https://github.com/youhavethewrong/initty"
+  :license {:name "Apache License, Version 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0"]
